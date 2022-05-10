9939c194505e67dd5d4ea3e0d9f15a9ac1446244
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-05-10 15:38:40 -0500

    added database and some modules

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener(
+    "stateChanged",
+    customEvent => {
+        render()
+    }
+)
+
+const render = () => {
+    fetchRequests().then(() => fetchPlumbers())
+    .then(() => fetchCompletions()).then(
+        () => {
+    mainContainer.innerHTML = SinkRepair()
+        }
+    )
+}
+
+render()
